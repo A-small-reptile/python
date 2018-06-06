5d7794ee893e983a51a62d65fd91eb1ecc161fc7
Arctic Caterpillar <37818751+A-small-reptile@users.noreply.github.com>
2018-06-06 16:21:27 +0800

    Delete 爬虫代理.py

--- amusing/smiple-spiders/爬虫代理.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-url = 'http://www.whatismyip.com.tw'
-proxy_support = urllib.request.ProxyHandler({'http':'122.72.18.35:80'})
-opener = urllib.request.build_opener(proxy_support)
-
-urllib.request.install_opener(opener)
-response = urllib.request.urlopen(url)
-html = response.read().decode('utf-8')
-print(html)
