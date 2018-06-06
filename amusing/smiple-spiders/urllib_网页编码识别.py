8c7683f852050a72ab6a88814e34a0c426462352
Arctic Caterpillar <37818751+A-small-reptile@users.noreply.github.com>
2018-06-06 16:20:58 +0800

    Delete urllib_网页编码识别.py

--- amusing/smiple-spiders/urllib_网页编码识别.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-import chardet
-input_url =input('请输入URL:')
-response = urllib.request.urlopen('http://'+input_url)
-html = response.read()
-decode_dict ={}
-decode_dict =chardet.detect(html)
-print('该网页使用的编码是:%s' % decode_dict['encoding'])
